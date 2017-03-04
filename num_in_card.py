376c549883fa513a62b9ac1255a9516e8ebad99b
Shatalin89 <Shatalin89@github.com>
2017-03-05 02:22:02 +0800

    add class Card

---
+++ num_in_card.py
@@ -0,0 +1,12 @@
+class NumInCard:
+
+    def __init__(self, x, y, num):
+        self.x = x
+        self.y = y
+        self.num = num
+
+    def get_num(self):
+        return self.num
+
+    def __str__(self):
+        return self.num
